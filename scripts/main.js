62e279e26451b7db3272c86c39aa25172f051c65
KarutaYT <tomatoy34@gmail.com>
2023-06-26 02:43:19 +0900

    a

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,13 @@ import {  world } from "@minecraft/server";
 import * as UI from "@minecraft/server-ui"
 
 world.afterEvents.itemUse.subscribe(ev => {
+    //遺物以外の追加アイテムでないkaroという名前空間の追加アイテム(=遺物)を右クリしたときフォームを出すためのibutuという関数を実行
     const { source , itemStack } = ev;
-    if(itemStack.typeId.startWith(`karo:`) && itemStack.typeId !== `karo:repair`) ibutu(source , itemStack);
+    if(itemStack.typeId.startsWith(`karo:`) && itemStack.typeId !== `karo:repair`) ibutu(source , itemStack);
 })
 
 /*
-ibutu関数について
+Dataクラスについて
 
 1行目
 name=遺物名
@@ -20,6 +21,8 @@ durability=消費する耐久力
 3行目
 ボタンに対応するコマンドたち(必ずキー名は0から始まる整数にすること(responce.selectionで整数が出力されるため))
 */
+
+
 class Data {
     c1 = [
       { name: "エジプトの葬儀師が宿ったナイフ" , "lore": "(C級 一般クラス/消耗性遺物)", "durability": "5"},
@@ -34,24 +37,38 @@ class Data {
     ];
   }
   
+//DataクラスをibutuDataとする
 const ibutuData = new Data()
 
+
+
 function ibutu(source , itemStack) {
+    //耐久値のコンポーネント取得
     const durability = itemStack.getComponent(`durability`)
+    //フォーム作成
     const form = new UI.ActionFormData()
+    //フォームのタイトル名にアイテム名代入
     form.title(`${ibutuData[itemStack.typeId.split(`:`)[1]][0].name}`)
+    //遺物の説明＆耐久値
     form.body(`${ibutuData[itemStack.typeId.split(`:`)[1]][0].lore}\n使用可能回数:(${durability.maxDurability - durability.damage}/${durability.maxDurability})`)
+    //Dataクラスのボタンのプロパティ数によってボタンを作成
     for(let key in ibutuData[itemStack.typeId.split(`:`)[1]][1]){
         form.button(`${ ibutuData[itemStack.typeId.split(`:`)[1]][1][key]}`)
     }
+    //フォームをプレイヤーに表示
     form.show(source).then(responce => {
+        //キャンセルされたら終了
         if(responce.canceled) return;
+        //押されたボタンによってコマンドを変える
         source.runCommandAsync(`${ibutuData[itemStack.typeId.split(`:`)[1]][2][responce.selection]}`)
-        //耐久度ダウン
+        //耐久度ダウンさせる 耐久値がまだある場合
         if(itemStack.getComponent(`durability`).damage < durability.maxDurability - Number(ibutuData[itemStack.typeId.split(`:`)[1]][0].durability)) {
+            //現在の耐久値をマイナス(負傷値を上げる) このときの数値はDataクラスから取る
             itemStack.getComponent(`durability`).damage += Number(ibutuData[itemStack.typeId.split(`:`)[1]][0].durability)
+            //アイテムの更新
             source.getComponent(`inventory`).container.setItem(source.selectedSlot , itemStack)
         } else {
+            //耐久のないアイテムを消す
             source.getComponent(`inventory`).container.setItem(source.selectedSlot)
         }
     })
