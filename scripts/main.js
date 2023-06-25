22e0a2e755d3fdcd91ab62a0178d554b5cfb671b
KarutaYT <tomatoy34@gmail.com>
2023-06-26 00:04:15 +0900

    a

--- scripts/main.js
+++ scripts/main.js
@@ -15,10 +15,11 @@ function C1(source , itemStack) {
     const durability = itemStack.getComponent(`durability`)
     new UI.ActionFormData()
     .title(`エジプトの葬儀師が宿ったナイフ`)
-    .body(`耐久力:${durability.maxDurability - durability.damage}`)
+    .body(`耐久力:${durability.maxDurability - durability.damage}\n ${itemStack.getComponent(`tekitou`).a}`)
     .button(`スピードアップ`)
     .button(`筋力アップ`)
     .show(source).then(responce => {
+        if(responce.canceled) return;
         if(responce.selection == 0) source.addEffect(`speed`, 100 );
         if(responce.selection == 1) source.addEffect(`strength`, 100 );
         //耐久度ダウン
