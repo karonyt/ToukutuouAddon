14a4f9e1a819c4036bbef39ef3b47f71278ffc79
KarutaYT <tomatoy34@gmail.com>
2023-06-25 23:08:40 +0900

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,24 @@
-import { world } from "@minecraft/server";
+import { Player, world } from "@minecraft/server";
+import * as UI from "@minecraft/server-ui"
 
-world.afterEvents.playerSpawn.subscribe(ev => {
-    world.sendMessage('a')
+world.afterEvents.useItem.subscribe(ev => {
+    const { source , itemStack } = ev;
+    switch(itemStack.typeId){
+        case 'karo:c-1': {
+            C1(source , itemStack);
+            break;
+        }
+    }
 })
+
+function C1(source , itemStack) {
+    new ActionFormData()
+    .title(`エジプトの葬儀師が宿ったナイフ`)
+    .button(`スピードアップ`)
+    .show(source).then(responce => {
+        if(responce.selection == 1) player.addEffect(`minecraft:speed`, 5 );
+
+        //耐久度ダウン
+        itemStack.triggerEvent(`karo:damage`)
+    })
+}
